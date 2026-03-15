574422de88f8b0e56b48e7d639bdec369cff5c1f
Anupama <kmanupama636@gmail.com>
2026-03-16 00:39:28 +0530

    fix allowed hosts

--- mihaa_backend/settings.py
+++ mihaa_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9i$jajt&$)m!+a!f$2v*3se*upje3h-#%5wd+0r3!kz@4lxmsa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mihaa-backend.onrender.com','127.0.0.1','localhost']
 
 
 # Application definition
